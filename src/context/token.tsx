6f47d06662974a318ccbfe042a9ed26e92875679
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-09-19 17:36:49 -0700

    feat(solid): add refresh

--- src/context/token.tsx
+++ src/context/token.tsx
@@ -4,12 +4,20 @@ import { createContext, createSignal, useContext } from "solid-js";
 const TokenContext = createContext<{
   token: Accessor<string>;
   setToken: Setter<string>;
-}>({ token: () => "", setToken: () => { } });
+  refresh: Accessor<string>;
+  setRefresh: Setter<string>;
+}>({
+  token: () => "",
+  setToken: () => {},
+  refresh: () => "",
+  setRefresh: () => {},
+});
 
 export const TokenProvider: ParentComponent = (props) => {
   const [token, setToken] = createSignal("");
+  const [refresh, setRefresh] = createSignal("");
   return (
-    <TokenContext.Provider value={{ token, setToken }}>
+    <TokenContext.Provider value={{ token, setToken, refresh, setRefresh }}>
       {props.children}
     </TokenContext.Provider>
   );
