2df55e378c24130c8405e29b08707745cca33e60
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-09-23 15:41:08 -0700

    style: mv fn

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,37 +3,6 @@ use base64::Engine;
 use serde_json::Value;
 
 #[tauri::command]
-async fn token(id: String, secret: String) -> Value {
-    let result = reqwest::Client::new()
-        .post("https://www.reddit.com/api/v1/access_token")
-        .header(
-            "Authorization",
-            format!(
-                "Basic {}",
-                base64::engine::general_purpose::STANDARD
-                    .encode(format!("{}:{}", id, secret).as_bytes())
-            ),
-        )
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .header("User-Agent", "Rocket/1.0 by Dosx001")
-        .body("grant_type=client_credentials")
-        .send()
-        .await;
-    match result {
-        Ok(response) => match response.json::<Value>().await {
-            Ok(value) => {
-                if let Some(token) = value.get("access_token") {
-                    return token.clone();
-                }
-            }
-            Err(_) => return Value::Null,
-        },
-        Err(_) => return Value::Null,
-    }
-    Value::Null
-}
-
-#[tauri::command]
 async fn auth(id: String, secret: String, code: String) -> Value {
     let result = reqwest::Client::new()
         .post("https://www.reddit.com/api/v1/access_token")
@@ -77,6 +46,37 @@ async fn hot(token: String, after: String) -> Value {
         },
         Err(_) => Value::Null,
     }
+}
+
+#[tauri::command]
+async fn token(id: String, secret: String) -> Value {
+    let result = reqwest::Client::new()
+        .post("https://www.reddit.com/api/v1/access_token")
+        .header(
+            "Authorization",
+            format!(
+                "Basic {}",
+                base64::engine::general_purpose::STANDARD
+                    .encode(format!("{}:{}", id, secret).as_bytes())
+            ),
+        )
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .header("User-Agent", "Rocket/1.0 by Dosx001")
+        .body("grant_type=client_credentials")
+        .send()
+        .await;
+    match result {
+        Ok(response) => match response.json::<Value>().await {
+            Ok(value) => {
+                if let Some(token) = value.get("access_token") {
+                    return token.clone();
+                }
+            }
+            Err(_) => return Value::Null,
+        },
+        Err(_) => return Value::Null,
+    }
+    Value::Null
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
